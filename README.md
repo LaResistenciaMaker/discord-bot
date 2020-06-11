a8689e47a013f10f0d27f8bea57a94d6988ba90c
vextraz <dvalenzuela3@uc.cl>
2020-06-11 15:07:57 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bot de discord de LR
+
+para colaborar, solicitar el archivo config.json a algún admin
---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const {
+    token
+} = require('./config.json');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    // client.user.setActivity(`En ${client.guilds.size} servidores!`);
+});
+
+client.on('message', msg => {
+
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+
+});
+
+client.login(token);
