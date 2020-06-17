f4a7fd4c6f2debe5a4f731e567c0f2ee490ae258
vextraz <dvalenzuela3@uc.cl>
2020-06-17 11:38:40 -0400

    getting tags

--- commands/tag.js
+++ commands/tag.js
@@ -1,9 +1,7 @@
 module.exports = {
     name: 'tag',
-    args: 1,
-    execute(message, args) {
-        console.log(args[0]);
+    execute(message) {
         let guild = message.guild.roles.cache;
-        message.channel.send(guild);
+        guild.each(role => message.author.send(role.name + role.id));
     }
 }
